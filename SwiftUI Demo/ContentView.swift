bcb8900b4fd9b0fde398d9107559cf1838142942
Wissa Michael <wissa@neugelb.com>
2020-07-26 00:45:57 +0200

    Initial Commit

---
+++ SwiftUI Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Demo
+//
+//  Created by Wissa Michael on 26.07.20.
+//  Copyright © 2020 Quedix. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
